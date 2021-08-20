05ba205c61a665a54d6412d5164efc1c2aeaff77
sandnima <46921550+sandnima@users.noreply.github.com>
2021-08-20 15:24:30 +0300

    Random word choice in better way

--- src/worte/views.py
+++ src/worte/views.py
@@ -1,4 +1,4 @@
-import random
+import random, sys
 from django.shortcuts import render
 from django.views.generic import View, ListView
 from django.shortcuts import get_object_or_404
@@ -42,13 +42,16 @@ class RandomChooseView(View):
         all_words = Substantiv.objects.all().values_list('id', flat=True)
         profile = Profile.objects.get(user=request.user)
         history_words = StimmtHistory.objects.filter(user=profile)
-        random_word = get_object_or_404(Substantiv, pk=weighted_choice(all_words=all_words, history=history_words))
+        # random_word = get_object_or_404(Substantiv, pk=weighted_choice(all_words=all_words, history=history_words))
+        weights = weight_list_generator(all_words=all_words, history=history_words)
+        # print(weights)
+        random_word = get_object_or_404(Substantiv, pk=random.choices(population=all_words, weights=weights)[0])
         
         # TESTING RANDOMNESS
         # def generator(bar):
         #     for k in range(bar):
         #         yield k
-        # test_bar = generator(100)
+        # test_bar = generator(1000)
         # test_result = dict()
         # list_gened = weight_list_generator(all_words=all_words, history=history_words)
         #
@@ -58,12 +61,14 @@ class RandomChooseView(View):
         #
         # print('---------------------')
         # for n in test_bar:
-        #     random = get_object_or_404(Substantiv, pk=weighted_choice(all_words=all_words, history=history_words))
+        #     w = weight_list_generator(all_words=all_words, history=history_words)
+        #     r = random.choices(population=all_words, weights=w)
+        #     random_word = get_object_or_404(Substantiv, pk=r[0])
         #     # random = weight_list_generator(all_words=all_words, history=history_words)
-        #     test_result[random] = test_result.get(random, 0) + 1
+        #     test_result[random_word] = test_result.get(random_word, 0) + 1
         # test_result = dict(sorted(test_result.items(), key=lambda item: item[1], reverse=True))
-        # for w, r in test_result.items():
-        #     print(w, r/100)
+        # for wrd, rnd in test_result.items():
+        #     print(wrd, rnd/1000)
         
         context = {
             'random_word': random_word,
@@ -72,26 +77,41 @@ class RandomChooseView(View):
         return render(request, template_name=self.template_name, context=context)
 
 
+# def weight_list_generator(all_words, history=None):
+#     base = 1
+#     for history_word in history:
+#         occ = history_word.mal
+#         base = base + occ
+#     for word_id in all_words:
+#         probability = base
+#         if word_id in history.values_list('wort', flat=True):
+#             probability = base - history.get(wort_id=word_id).mal
+#         yield probability
+
+
 def weight_list_generator(all_words, history=None):
     base = 1
     for history_word in history:
         occ = history_word.mal
         base = base + occ
+    weight_list = []
     for word_id in all_words:
         probability = base
         if word_id in history.values_list('wort', flat=True):
             probability = base - history.get(wort_id=word_id).mal
-        yield word_id, probability
+        weight_list.append(probability)
+    min_weight = min(weight_list) - 1
+    weight_list = [weight-min_weight for weight in weight_list]
+    return weight_list
 
-
-def weighted_choice(all_words, history=()):
-    total = 1
-    for occ in history:
-        total = total + occ.mal
-    total = total * len(all_words) - (total - 1)
-    for word, probability in weight_list_generator(all_words, history):
-        rand = random.random()
-        prob = 1 / total * probability
-        total = total - probability
-        if rand < prob:
-            return word
+# def weighted_choice(all_words, history=()):
+#     total = 1
+#     for occ in history:
+#         total = total + occ.mal
+#     total = total * len(all_words) - (total - 1)
+#     for word, probability in weight_list_generator(all_words, history):
+#         rand = random.random()
+#         prob = 1 / total * probability
+#         total = total - probability
+#         if rand < prob:
+#             return word
